ebb8f103a1f55708aaa6dd3594e631afa6971e73
Soumya <soumyasen1809@gmail.com>
2024-08-29 11:10:08 +0200

    [4] Fixed issue with Grayscale operation taking long
    - Using pix value in the iterator instead of calling
    the entire path through self.image... for the
    alpha value
    
    % SOFTWARE
    Fixes issue #4

--- lib/filters/gray_scale.rs
+++ lib/filters/gray_scale.rs
@@ -1,4 +1,4 @@
-use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use crate::core::{image::Images, operations::Operation, pixel::Pixels};
 
@@ -10,15 +10,14 @@ pub enum GrayScaleAlgorithms {
 
 fn select_grayscale_algorithm(algo: &GrayScaleAlgorithms, pix: &Pixels) -> u8 {
     match algo {
-        // average grayscale algorithm
         GrayScaleAlgorithms::Average => (pix.get_red() + pix.get_green() + pix.get_blue()) / 3,
 
         GrayScaleAlgorithms::Luminosity => {
             // Luminosity method: https://www.mathworks.com/help/matlab/ref/rgb2gray.html
-            (((pix.get_red() as f64 * 0.299) as u8)
-                + ((pix.get_green() as f64 * 0.5879) as u8)
-                + (pix.get_blue() as f64 * 0.114) as u8)
-                / 3
+            (((pix.get_red() as f64 * 0.299)
+                + (pix.get_green() as f64 * 0.5879)
+                + (pix.get_blue() as f64 * 0.114))
+                / 3.0) as u8
         }
     }
 }
@@ -39,24 +38,20 @@ impl GrayScale {
 
 impl Operation for GrayScale {
     fn apply(&self) -> Images {
-        let grayscale_pixel: Vec<u8> = self
+        let new_pixel: Vec<Pixels> = self
             .image
             .get_image()
-            .par_iter()
-            .map(|pix| select_grayscale_algorithm(&self.algo, pix))
-            .collect();
-
-        let new_pixel: Vec<Pixels> = (0..self.image.get_image().len())
             .into_par_iter()
-            .map(|index| {
+            .map(|pix| {
+                let grayscale_pixel = select_grayscale_algorithm(&self.algo, &pix);
                 Pixels::new(
-                    *grayscale_pixel.get(index).unwrap(),
-                    *grayscale_pixel.get(index).unwrap(),
-                    *grayscale_pixel.get(index).unwrap(),
-                    (*self.image.get_image().get(index).unwrap()).get_alpha(),
+                    grayscale_pixel,
+                    grayscale_pixel,
+                    grayscale_pixel,
+                    pix.get_alpha(),
                 )
             })
-            .collect();
+            .collect::<Vec<Pixels>>();
 
         let new_image = Images::new(
             self.image.get_width(),

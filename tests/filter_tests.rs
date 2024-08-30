c733206c1896cc385eae8802d47664274d3aa6ef
Soumya <soumyasen1809@gmail.com>
2024-08-30 09:46:25 +0200

    [5] Restructured tests for filter
    - Added common setup for test
    
    %UNIT_TEST

--- tests/filter_tests.rs
+++ tests/filter_tests.rs
@@ -1,3 +1,56 @@
+use image_processor::core::{image::Images, pixel::Pixels};
+
+fn common_steup_simple() -> (Pixels, Images) {
+    let pix = Pixels::new(100, 100, 100, 255);
+    let img = Images::new(3, 3, 3, vec![pix.clone(); 9]);
+
+    (pix, img)
+}
+
+fn common_steup_complex() -> (Pixels, Images) {
+    let pix = Pixels::new(100, 100, 100, 255);
+    let img = Images::new(
+        3,
+        3,
+        3,
+        vec![
+            Pixels::new(255, 0, 0, 255),
+            Pixels::new(0, 255, 0, 255),
+            Pixels::new(0, 0, 255, 255),
+            Pixels::new(255, 255, 255, 255),
+            Pixels::new(255, 0, 0, 255),
+            Pixels::new(0, 255, 0, 255),
+            Pixels::new(0, 0, 255, 255),
+            Pixels::new(255, 255, 255, 255),
+            Pixels::new(255, 0, 255, 255),
+        ],
+    );
+
+    (pix, img)
+}
+
+fn common_steup_complex_varied() -> (Pixels, Images) {
+    let pix = Pixels::new(100, 100, 100, 255);
+    let img = Images::new(
+        3,
+        3,
+        3,
+        vec![
+            Pixels::new(155, 0, 0, 155),
+            Pixels::new(0, 155, 155, 155),
+            Pixels::new(100, 120, 130, 255),
+            Pixels::new(110, 125, 135, 255),
+            Pixels::new(120, 130, 140, 255),
+            Pixels::new(255, 0, 0, 155),
+            Pixels::new(0, 0, 155, 155),
+            Pixels::new(155, 0, 255, 155),
+            Pixels::new(155, 0, 155, 155),
+        ],
+    );
+
+    (pix, img)
+}
+
 #[cfg(test)]
 mod tests {
     use image_processor::{
@@ -10,10 +63,12 @@ mod tests {
         },
     };
 
+    use super::*;
+
     #[test]
     fn blur_filter_box_blur_test() {
-        let pix = Pixels::new(100, 100, 100, 255);
-        let img = Images::new(3, 3, 3, vec![pix; 9]);
+        let (_, img) = common_steup_simple();
+
         let blurred_img = Blur::new(&img, SmoothingKernelChoices::BoxBlur).apply();
         let new_image = Images::new(1, 1, 3, vec![Pixels::new(44, 44, 44, 255)]);
         assert_eq!(blurred_img, new_image);
@@ -22,22 +77,7 @@ mod tests {
     #[test]
     fn blur_filter_box_blur_bigger_test() {
         // Create a sample image
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(255, 0, 0, 255),
-                Pixels::new(0, 255, 0, 255),
-                Pixels::new(0, 0, 255, 255),
-                Pixels::new(255, 255, 255, 255),
-                Pixels::new(255, 0, 0, 255),
-                Pixels::new(0, 255, 0, 255),
-                Pixels::new(0, 0, 255, 255),
-                Pixels::new(255, 255, 255, 255),
-                Pixels::new(255, 0, 255, 255),
-            ],
-        );
+        let (_, img) = common_steup_complex();
 
         // Apply the blur filter
         let blurred_img = Blur::new(&img, SmoothingKernelChoices::BoxBlur).apply();
@@ -49,8 +89,7 @@ mod tests {
 
     #[test]
     fn blur_filter_gaussian_kernel_test() {
-        let pix = Pixels::new(100, 100, 100, 255);
-        let img = Images::new(3, 3, 3, vec![pix; 9]);
+        let (_, img) = common_steup_simple();
 
         let blurred_img = Blur::new(&img, SmoothingKernelChoices::Gaussian).apply();
         let new_image = Images::new(1, 1, 3, vec![Pixels::new(56, 56, 56, 255)]);
@@ -60,22 +99,7 @@ mod tests {
     #[test]
     fn blur_filter_gaussian_kernel_bigger_test() {
         // Create a sample image
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(255, 0, 0, 255),
-                Pixels::new(0, 255, 0, 255),
-                Pixels::new(0, 0, 255, 255),
-                Pixels::new(255, 255, 255, 255),
-                Pixels::new(255, 0, 0, 255),
-                Pixels::new(0, 255, 0, 255),
-                Pixels::new(0, 0, 255, 255),
-                Pixels::new(255, 255, 255, 255),
-                Pixels::new(255, 0, 255, 255),
-            ],
-        );
+        let (_, img) = common_steup_complex();
 
         // Apply the blur filter
         let blurred_img = Blur::new(&img, SmoothingKernelChoices::Gaussian).apply();
@@ -87,47 +111,17 @@ mod tests {
 
     #[test]
     fn edge_detection_filter_outline_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 155, 0, 155),
-                Pixels::new(0, 155, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let edge_detection_image =
             EdgeDetection::new(&img, EdgeDetectingKernelChoices::Outline).apply();
-        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 255, 0, 255)]);
+        let new_image = Images::new(1, 1, 3, vec![Pixels::new(30, 255, 135, 255)]);
         assert_eq!(edge_detection_image, new_image);
     }
 
     #[test]
     fn edge_detection_filter_sobelx_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(100, 120, 130, 255),
-                Pixels::new(110, 125, 135, 255),
-                Pixels::new(120, 130, 140, 255),
-                Pixels::new(255, 0, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 0, 255, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let edge_detection_image =
             EdgeDetection::new(&img, EdgeDetectingKernelChoices::SobelX).apply();
@@ -137,123 +131,48 @@ mod tests {
 
     #[test]
     fn edge_detection_filter_sobely_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 155, 0, 155),
-                Pixels::new(0, 155, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let edge_detection_image =
             EdgeDetection::new(&img, EdgeDetectingKernelChoices::SobelY).apply();
-        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 0, 155, 255)]);
+        let new_image = Images::new(1, 1, 3, vec![Pixels::new(210, 0, 255, 255)]);
         assert_eq!(edge_detection_image, new_image);
     }
 
     #[test]
     fn edge_detection_filter_emboss_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 155, 0, 155),
-                Pixels::new(0, 155, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let edge_detection_image =
             EdgeDetection::new(&img, EdgeDetectingKernelChoices::Emboss).apply();
-        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 255, 255, 255)]);
+        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 0, 255, 255)]);
         assert_eq!(edge_detection_image, new_image);
     }
 
     #[test]
     fn sharpen_filter_basic_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 155, 0, 155),
-                Pixels::new(0, 155, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let sharpen_image = Sharpen::new(&img, SharpeningKernelChoices::Basic).apply();
-        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 155, 0, 255)]);
+        let new_image = Images::new(1, 1, 3, vec![Pixels::new(80, 255, 155, 255)]);
         assert_eq!(sharpen_image, new_image);
     }
 
     #[test]
     fn sharpen_filter_highpass_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 155, 0, 155),
-                Pixels::new(0, 155, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let sharpen_image = Sharpen::new(&img, SharpeningKernelChoices::HighPass).apply();
-        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 255, 0, 255)]);
+        let new_image = Images::new(1, 1, 3, vec![Pixels::new(30, 255, 135, 255)]);
         assert_eq!(sharpen_image, new_image);
     }
 
     #[test]
     fn sharpen_filter_edge_enhancement_test() {
-        let img = Images::new(
-            3,
-            3,
-            3,
-            vec![
-                Pixels::new(155, 0, 0, 155),
-                Pixels::new(0, 155, 155, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 155, 0, 155),
-                Pixels::new(0, 155, 0, 155),
-                Pixels::new(0, 0, 155, 155),
-                Pixels::new(155, 155, 155, 155),
-                Pixels::new(155, 0, 155, 155),
-            ],
-        );
+        let (_, img) = common_steup_complex_varied();
 
         let sharpen_image = Sharpen::new(&img, SharpeningKernelChoices::EdgeEnhancement).apply();
-        let new_image = Images::new(1, 1, 3, vec![Pixels::new(255, 255, 0, 255)]);
+        let new_image = Images::new(1, 1, 3, vec![Pixels::new(150, 255, 255, 255)]);
         assert_eq!(sharpen_image, new_image);
     }
 

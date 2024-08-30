1341a55fa88a19e5b7d6da56e7fbb17583d385d4
Soumya <soumyasen1809@gmail.com>
2024-08-30 23:05:24 +0200

    [6] Added unit test for HSV converter
    - Added unit test for rb to hsv converter
    
    %UNIT_TEST

--- src/utils/color_space_converter.rs
+++ src/utils/color_space_converter.rs
@@ -3,9 +3,9 @@ use crate::core::image::Images;
 pub fn rgba_to_hsv(image: Images) -> Vec<(f64, f64, f64)> {
     let mut hsv_image: Vec<(f64, f64, f64)> = Vec::new();
     for pix in image.get_image() {
-        let r = pix.get_red() as f64;
-        let g = pix.get_green() as f64;
-        let b = pix.get_blue() as f64;
+        let r = pix.get_red() as f64 / 255.0;
+        let g = pix.get_green() as f64 / 255.0;
+        let b = pix.get_blue() as f64 / 255.0;
 
         let min_val = (r.min(g)).min(b);
         let max_val = (r.max(g)).max(b);
@@ -17,9 +17,9 @@ pub fn rgba_to_hsv(image: Images) -> Vec<(f64, f64, f64)> {
             hsv.1 = 0.0;
             hsv.2 = max_val as f64;
         } else {
-            if max_val == hsv.0 {
+            if max_val == r {
                 hsv.0 = 60.0 * (g - b) / delta;
-            } else if max_val == hsv.1 {
+            } else if max_val == g {
                 hsv.0 = 60.0 * (b - r) / delta + 120.0;
             } else {
                 hsv.0 = 60.0 * (r - g) / delta + 240.0;
@@ -33,7 +33,7 @@ pub fn rgba_to_hsv(image: Images) -> Vec<(f64, f64, f64)> {
             hsv.2 = max_val;
         }
 
-        hsv_image.push((hsv.0, hsv.1 * 255.0, hsv.2 * 255.0));
+        hsv_image.push((hsv.0, hsv.1, hsv.2));
     }
 
     hsv_image
